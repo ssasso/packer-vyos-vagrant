2e8e9866c42a38e41ef0fcd3be14de67708ec8a0
Stefano Sasso <stefano.sasso@hpe.com>
2024-04-29 09:54:58 +0200

    Updates for VyOS 1.5
    Fix #2

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+wget -c -O vyos-rolling-latest.iso $(wget -qO - https://raw.githubusercontent.com/vyos/vyos-rolling-nightly-builds/main/version.json | jq -r '.[].url')
+
 cksum=$(sha1sum vyos-rolling-latest.iso | awk '{print $1}')
 
 cat vyos.json | jq --arg cksum "$cksum" '.builders[0] += { "iso_checksum": $cksum }' > vyos-build.json
